5a13d8dd9fa7e9cec959aefa8802c3e2582f48db
gamescril <npeulic4@gmail.com>
2024-11-30 00:54:41 +0100

    Prepare for Railway deployment

--- db.js
+++ db.js
@@ -2,10 +2,11 @@ const mysql = require('mysql2/promise');
 require('dotenv').config();
 
 const pool = mysql.createPool({
-    host: process.env.MYSQL_HOST,
-    user: process.env.MYSQL_USER,
-    password: process.env.MYSQL_PASSWORD,
-    database: process.env.MYSQL_DATABASE,
+    host: process.env.MYSQL_HOST || process.env.MYSQLHOST,
+    user: process.env.MYSQL_USER || process.env.MYSQLUSER,
+    password: process.env.MYSQL_PASSWORD || process.env.MYSQLPASSWORD,
+    database: process.env.MYSQL_DATABASE || process.env.MYSQLDATABASE,
+    port: process.env.MYSQL_PORT || process.env.MYSQLPORT || 3306,
     waitForConnections: true,
     connectionLimit: 10,
     queueLimit: 0,
